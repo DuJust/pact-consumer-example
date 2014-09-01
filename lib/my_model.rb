bd92c03870387c9b417bbda8e8d3e2d2d68d4f8b
DuJust <du.just.it@gmail.com>
2014-09-01 19:57:12 +1000

    add grape api.

--- lib/my_model.rb
+++ lib/my_model.rb
@@ -1,11 +1,19 @@
-class MyModel
-  attr_reader :name
+module PactExample
+  class MyModel
+    attr_accessor :name
 
-  def initialize(name)
-    @name = name
-  end
+    def initialize(name)
+      @name = name
+    end
 
-  def ==(other)
-    other.is_a?(MyModel) && other.name == name
+    def ==(other)
+      other.is_a?(MyModel) && other.name == name
+    end
+
+    def to_json
+      {
+        name: @name
+      }
+    end
   end
 end
--- spec/lib/my_model_spec.rb
+++ spec/lib/my_model_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 require 'my_model'
 
-describe MyModel do
-  describe '#==' do
-    subject { MyModel.new('new_one') }
-    it { should eq(MyModel.new('new_one')) }
+module PactExample
+  describe MyModel do
+    describe '#==' do
+      subject { MyModel.new('new_one') }
+      it { should eq(MyModel.new('new_one')) }
+    end
   end
 end
